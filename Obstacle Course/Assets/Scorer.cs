3fd89fbdc3e294e0f12f80152749a4f17095359b
dnicholson0011 <81931232+dnicholson0011@users.noreply.github.com>
2022-09-16 18:50:16 -0500

    Changes

--- Obstacle Course/Assets/Scorer.cs
+++ Obstacle Course/Assets/Scorer.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectHit : MonoBehaviour
+public class Scorer : MonoBehaviour
 {
+
+
     private void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
+        Debug.Log("You've bumped into a thing this many times: ");
 
     }
 }
