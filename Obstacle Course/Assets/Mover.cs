643b722b2ad0cb613f04b0b2934786fd3df82ae7
dnicholson0011 <81931232+dnicholson0011@users.noreply.github.com>
2022-09-16 15:33:10 -0500

    Update Mover.cs

--- Obstacle Course/Assets/Mover.cs
+++ Obstacle Course/Assets/Mover.cs
@@ -17,16 +17,23 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
-        transform.Translate(xValue, 0, zValue);
+        MovePlayer();
 
     }
 
     void PrintInstructions()
     {
-        Debug.Log("Welcome to the jungle!!");
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
 
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        transform.Translate(xValue, 0, zValue);
     }
 }
